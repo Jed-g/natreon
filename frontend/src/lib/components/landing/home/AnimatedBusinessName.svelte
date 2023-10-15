02ba953e1bed35b40949c2fa4846e6e24e23b311
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2023-10-15 10:22:39 +0100

    Update AnimatedBusinessName.svelte

--- frontend/src/lib/components/landing/home/AnimatedBusinessName.svelte
+++ frontend/src/lib/components/landing/home/AnimatedBusinessName.svelte
@@ -1,12 +1,12 @@
-<h2 class="relative">
+<h2 class="relative font-mono">
 	<span class="text-1 text-4xl">Natreon&#8482;</span>
-	<span class="text-2 text-4xl">Go Explore.</span>
+	<span class="text-2 text-3xl">Trek into the wild.</span>
 </h2>
 
 <style>
 	.text-1 {
 		animation: text1;
-		animation-timing-function: steps(20, end);
+		animation-timing-function: steps(16, end);
 	}
 
 	.text-2 {
@@ -27,7 +27,7 @@
 
 	.text-1::after,
 	.text-2::after {
-		content: '|';
+		content: '▐';
 		position: absolute;
 		right: 0;
 		animation: caret infinite;
@@ -43,7 +43,7 @@
 		}
 		10%,
 		40% {
-			width: 4.8em;
+			width: 4.9em;
 		}
 	}
 
@@ -56,7 +56,7 @@
 
 		60%,
 		90% {
-			width: 5.4em;
+			width: 10.8em;
 		}
 	}
 
