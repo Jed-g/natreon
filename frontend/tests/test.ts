70ba1cdd5bc79ac2646244059cc917f2528124b1
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2023-10-20 01:36:18 +0100

    Write tests for frontend

--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -1,6 +1,20 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('login button visible', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
+
+test('animated logo visible after navigation: Home -> Login -> Home', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Login' }).click();
+	await page.getByRole('button', { name: 'Home' }).click();
+	await expect(page.getByRole('img')).toBeVisible();
+});
+
+test('signup header visible after navigation: Home -> Login -> Signup', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Login' }).click();
+	await page.getByRole('link', { name: 'Create An Account' }).click();
+	await expect(page.getByRole('heading', { name: 'Signup' })).toBeVisible();
 });
