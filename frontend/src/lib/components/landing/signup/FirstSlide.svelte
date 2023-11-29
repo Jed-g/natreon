25789451c37b15723fbf0113de383e0531483edb
aca20rk <rebanthk@LAPTOP-3961N1J3.>
2023-11-29 12:55:44 +0000

    Front end for CTA(signup) page TO DO backend mailing list work

--- frontend/src/lib/components/landing/signup/FirstSlide.svelte
+++ frontend/src/lib/components/landing/signup/FirstSlide.svelte
@@ -2,6 +2,19 @@
 	class="flex flex-col h-full items-center justify-center relative select-none background-image"
 	id="home"
 >
+	<div class="hero min-h-screen">
+		<div class="hero-content text-center">
+			<div class="max-w-2xl">
+				<h1 class="text-5xl font-bold">Register your interest here!</h1>
+				<p class="py-6">
+					Be among the first to experience our solution and keep up to date with our progress by
+					filling in your email below!
+				</p>
+				<input type="text" placeholder="Your email Here" class="input input-bordered w-full mb-3" />
+				<button class="btn btn-primary">Register My Interest!</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
