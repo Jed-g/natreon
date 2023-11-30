fd595a21f18eb2a2302f0b42c85e241fc7f434e7
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2023-11-30 22:20:42 +0000

    Update DSN inside config/initializers/sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 Sentry.init do |config|
   # Sentry is only enabled when the dsn is set.
-  config.dsn = "REPLACE_WITH_YOUR_DSN" unless Rails.env.development? || Rails.env.test?
+  config.dsn = "https://67b349f804b72c02786d1aea5a301859@sentry.shefcompsci.org.uk/365" unless Rails.env.development? || Rails.env.test?
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.before_send = lambda {|event, _hint|
     ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(event.to_hash)
