203026475dce10fd21497f57db5147ddf83db543
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2023-12-01 06:40:07 +0000

    Make 'webpack' command available during deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -129,3 +129,15 @@ Rake::Task["deploy:assets:backup_manifest"].clear_actions
 ## Restart delayed_job during the deployment process
 after  "deploy:updated",  "delayed_job:stop"
 before "deploy:finished", "delayed_job:start"
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
