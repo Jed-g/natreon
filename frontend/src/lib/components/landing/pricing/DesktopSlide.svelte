2b3793d1cb3c6340e12245b1fc02230dbb329d8c
Harry Akrill <hakrill1@sheffield.ac.uk>
2023-12-06 17:03:26 +0000

    Edit button popup

--- frontend/src/lib/components/landing/pricing/DesktopSlide.svelte
+++ frontend/src/lib/components/landing/pricing/DesktopSlide.svelte
@@ -113,15 +113,4 @@
     box-sizing: border-box;
     display: block; 
   }
-  .popup {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 20px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    z-index: 999;
-  }
 </style>
