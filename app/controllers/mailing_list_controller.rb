a5629be2a1f384b661af762cfdf22f32459b77fe
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2023-12-09 01:07:10 +0000

    Add CORS to fix 'manifest.json blocked by CORS policy' error

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -1,35 +1,34 @@
+# frozen_string_literal: true
+
 class MailingListController < ApplicationController
+  def submit_email
+    read_email_param
 
-    def submit_email
-        read_email_param
+    return render_bad_request("Email is invalid") unless valid_email?(@email)
 
-        return render_bad_request("Email is invalid") unless valid_email?(@email)
+    return render_bad_request("Email has already been recorded") if MailingList.exists?(email: @email)
 
-        if MailingList.exists?(email: @email)
-            return render_bad_request("Email has already been recorded")
-        end
+    mailing_list_item = MailingList.new(email: @email)
+    return render_bad_request("Email is invalid") unless mailing_list_item.valid?
 
-        mailing_list_item = MailingList.new(email: @email)
-        return render_bad_request("Email is invalid") unless mailing_list_item.valid?
+    mailing_list_item.save
+    render json: {message: "Email recorded successfully"}
+  end
 
-        mailing_list_item.save
-        render json: {message: "Email recorded successfully"}
-    end
+  private
 
-    private
+  def valid_email?(email_)
+    email_.match(EMAIL_REGEX)
+  end
 
-    def valid_email?(email_)
-        email_.match(EMAIL_REGEX)
-      end  
+  def read_email_param
+    @email = params[:email]
+    render_bad_request if @email.nil?
+  end
 
-    def read_email_param
-        @email = params[:email]
-        render_bad_request if @email.nil?
-    end
+  def render_bad_request(message)
+    render json: {message:}, status: :bad_request
+  end
 
-    def render_bad_request(message)
-        render json: {message: message}, status: :bad_request
-    end
-
-    EMAIL_REGEX = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/
-end 
+  EMAIL_REGEX = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/
+end
