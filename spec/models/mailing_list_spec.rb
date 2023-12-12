fbe5696fa17aa9ffe83bd660361d9e63749c76be
aca20rk <rkanner1@sheffield.ac.uk>
2023-12-12 21:39:50 +0000

    deployed files update

--- spec/models/mailing_list_spec.rb
+++ spec/models/mailing_list_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: mailing_list
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "rails_helper"
 
 RSpec.describe MailingList do
