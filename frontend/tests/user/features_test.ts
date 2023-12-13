1b6a7762363518b58c806737ceb2e408dacc9c06
Harry Akrill <hakrill1@sheffield.ac.uk>
2023-12-13 19:34:49 +0000

    Yarn deploy run and features_test feature test file created

---
+++ frontend/tests/user/features_test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'I Understand' }).click();
+  await page.locator('label').filter({ hasText: 'Home' }).locator('svg').click();
+  await page.getByRole('button', { name: 'Features' }).click();
+  await page.locator('#features-desktop').getByRole('checkbox').first().check();
+  await expect(page.locator('#features-desktop').getByText('Be guided by the interactive map to your new favourite chill out spot! 🧘‍♀️')).toBeVisible();
+});
