1c346634d7782e4f41476a847eb03a7a214dd1d6
Harry Akrill <hakrill1@sheffield.ac.uk>
2023-12-13 21:59:52 +0000

    Various spec files added

---
+++ spec/serializers/user_serializer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  jti                    :string           not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  user_type              :integer          default("customer"), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_jti                   (jti) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+require "rails_helper"
+
+RSpec.describe UserSerializer do
+    
+end
