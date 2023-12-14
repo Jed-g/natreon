1712c014497f914fdbd21e3ba510a1a639013e76
aca20rk <rkanner1@sheffield.ac.uk>
2023-12-14 03:29:07 +0000

    mailing_list controller rspec tests for admin and user

---
+++ spec/controllers/admin/mailing_list_controller_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe Admin::MailingListController do
+    let(:admin) { create(:user, user_type: "admin") }
+    let(:user) { create(:user, user_type: "customer") }
+
+    before do
+        allow(controller).to receive(:current_user).and_return(current_user)
+    end
+
+    describe "GET #index" do
+    context "when the user is an admin" do
+      let(:current_user) { admin }
+
+      before do
+        get :all_emails
+      end
+
+      it "returns a 200 response" do
+        expect(response).to have_http_status :ok
+      end
+    end
+
+    context "when the user is not an admin" do
+      let(:current_user) { user }
+
+      before do
+        get :all_emails
+      end
+
+      it "returns a 401 response" do
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+  end
+
+end
---
+++ spec/controllers/mailing_list_controller_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe MailingListController do 
+    let(:email) { create(:mailinglist, email: "test@test.com") }
+
+    describe "#submit_email" do
+        context 'with  a valid email' do
+            let(:valid_email) {'test_email@test.com'}
+
+            it 'creates a new mailinglist item and returns a success message' do
+                post :submit_email, params:{email: valid_email}
+                expect(response).to have_http_status :ok
+                json_response = JSON.parse(response.body)
+
+                expect(json_response).to have_key('message')
+                expect(json_response['message']).to eq('Email recorded successfully')
+            end
+        end
+
+        context 'with a invalid email' do
+            let(:invalid_email) {'test_emailtest.com'}
+
+            it 'returns an error message' do
+                post :submit_email, params:{email: invalid_email}
+                expect(response).to have_http_status(:bad_request)
+            end
+
+            it 'does not create a new mailinglist item' do
+                expect {
+                  post :submit_email, params: { email: invalid_email }
+                }.not_to change(MailingList, :count)
+            end
+        end
+    end
+end
