2141f796e3b8edcea3f40791b84aeffc2230744c
Ben Barrow <babarrow1@sheffield.ac.uk>
2023-12-14 15:08:02 -0500

    adjust laout of mobile features slide, add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,49 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+case Rails.env
+when "test"
+    User.find_or_create_by(email: "test@user.com") do |user|
+        user.password = "password"
+        user.user_type = 0
+      end
+    User.find_or_create_by(email: "test@admin.com") do |user|
+        user.password = "password"
+        user.user_type = 1
+    end
+    User.find_or_create_by(email: "test@reporter.com") do |user|
+        user.password = "password"
+        user.user_type = 2
+    end
+    Review.create(rating: 4, content:"trial_content_1", author:"author_1", upvotes: 10, downvotes:2)
+    Review.create(rating: 4, content:"trial_content_2", author:"author_2", upvotes: 10, downvotes:2)
+    Question.create(answer:nil , question:"test_question_1", upvotes:0, downvotes:0)
+    Question.create(answer:"test_answer_2" , question:"test_question_2", upvotes:0, downvotes:0)
+
+when "development"
+    User.find_or_create_by(email: "test@user.com") do |user|
+        user.password = "password"
+        user.user_type = 0
+      end
+    User.find_or_create_by(email: "test@admin.com") do |user|
+        user.password = "password"
+        user.user_type = 1
+    end
+    User.find_or_create_by(email: "test@reporter.com") do |user|
+        user.password = "password"
+        user.user_type = 2
+    end
+
+when "production"
+
+when "demo"
+    User.find_or_create_by(email: "test@user.com") do |user|
+        user.password = "password"
+        user.user_type = 0
+      end
+    User.find_or_create_by(email: "test@admin.com") do |user|
+        user.password = "password"
+        user.user_type = 1
+    end
+    User.find_or_create_by(email: "test@reporter.com") do |user|
+        user.password = "password"
+        user.user_type = 2
+    end
+end
--- frontend/src/lib/components/landing/features/MobileSlide.svelte
+++ frontend/src/lib/components/landing/features/MobileSlide.svelte
@@ -6,9 +6,8 @@
 	import { MINIMUM_TIME_ON_SLIDE_FOR_PATH_REGISTRATION_MS } from '$lib/config';
 	import { pathToRegistrationAppend } from '$lib/utils';
 	import { onDestroy, onMount } from 'svelte';
-
+	import { goto } from '$app/navigation';
 	let timeout: ReturnType<typeof setTimeout>;
-
 	$: {
 		if (intersecting) {
 			timeout = setTimeout(() => {
@@ -20,44 +19,50 @@
 			clearTimeout(timeout);
 		}
 	}
-
 	let element: HTMLDivElement;
 	let intersecting: boolean;
-
 	let carousel: HTMLDivElement;
 	let currentIndex = 0;
 	let interval: any;
-
 	const description = [
-		"Be guided by the interactive map to your new favourite chill out spot! 🧘‍♀️",
-		"Learn more about your local environment with hand-curated Points of Interest! 🗿",
+		'Be guided by the interactive map to your new favourite chill out spot! 🧘‍♀️',
+		'Learn more about your local environment with hand-curated Points of Interest! 🗿',
 		"Share your favourite spots, plan events and see what's happening in your area with Natreon-𝕏. 🫶"
-	]
-
+	];
+	const goToSlide = (index: number) => {
+		switch (index) {
+			case 1:
+				goto('#slide2');
+				break;
+			case 2:
+				goto('#slide3');
+				break;
+			default:
+				goto('#slide1');
+				break;
+		}
+	};
 	function next() {
 		currentIndex = (currentIndex + 1) % 3;
-		console.log('CLICK-NEXT', currentIndex);
+		goToSlide(currentIndex);
+		//console.log('CLICK-NEXT', currentIndex+1);
 	}
-
 	function prev() {
 		currentIndex = (currentIndex - 1 + 3) % 3;
-		console.log('CLICK-PREV', currentIndex);
+		goToSlide(currentIndex);
+		//console.log('CLICK-PREV', currentIndex+1);
 	}
-
 	function startAutoScroll() {
 		interval = setInterval(() => {
 			next();
 		}, 5000);
 	}
-
 	function stopAutoScroll() {
 		clearInterval(interval);
 	}
-
 	onMount(() => {
 		startAutoScroll();
 	});
-
 	onDestroy(() => {
 		stopAutoScroll();
 	});
@@ -69,28 +74,42 @@
 		id="home"
 		bind:this={element}
 	>
-		<div class="carousel-container">
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<div
-				class="w-5/6 carousel rounded-box"
-				bind:this={carousel}
-				on:mouseenter={stopAutoScroll}
-				on:mouseleave={startAutoScroll}
-			>
-				<div class="carousel-item w-full">
-					<img src={MapFeatureImg} alt="Interactive Map!" />
+			<div class="carousel-container">
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					class="w-5/6 carousel rounded-box"
+					bind:this={carousel}
+					on:mouseenter={stopAutoScroll}
+					on:mouseleave={startAutoScroll}
+				>
+					<div id="slide1" class="carousel-item w-full">
+						<img src={MapFeatureImg} alt="Interactive Map!" />
+					</div>
+					<div id="slide2" class="carousel-item w-full">
+						<img src={POIFeatureImg} alt="Points of interest!" />
+					</div>
+					<div id="slide3" class="carousel-item w-full">
+						<img src={SocialFeatureImg} alt="Social Features!" />
+					</div>
 				</div>
-				<div class="carousel-item w-full">
-					<img src={POIFeatureImg} alt="Points of interest!" />
-				</div>
-				<div class="carousel-item w-full">
-					<img src={SocialFeatureImg} alt="Social Features!" />
-				</div>
+				<div id="feature-description" class="mt-4 text-lg font-bold">{description[currentIndex]}</div>
+				<button
+					on:click={(e) => !e.isTrusted && prev()}
+					on:click|trusted={() => {
+						stopAutoScroll();
+						prev();
+					}}
+					class="nav-button nav-button-left">←</button
+				>
+				<button
+					on:click={(e) => !e.isTrusted && next()}
+					on:click|trusted={() => {
+						stopAutoScroll();
+						next();
+					}}
+					class="nav-button nav-button-right">→</button
+				>
 			</div>
-			<div id="feature-description" class="mt-4 text-lg font-bold">{description[currentIndex]}</div>
-			<button on:click={prev} class="nav-button nav-button-left">←</button>
-			<button on:click={next} class="nav-button nav-button-right">→</button>
-		</div>
 	</div>
 </IntersectionObserver>
 
@@ -98,5 +117,8 @@
 	.background-image {
 		background-image: url('/nature.jpg');
 		background-size: cover;
+	}
+	.carousel-container {
+		@apply flex flex-col items-center justify-center mt-16;
 	}
 </style>
