b484f570d7542d1b1ebfe474f836937dd40fc7ea
aca20rk <rkanner1@sheffield.ac.uk>
2024-04-12 17:59:16 +0100

    Added description and features to POI card, navigate button redirects to google maps

--- frontend/src/lib/components/app/main/home/map/Map.svelte
+++ frontend/src/lib/components/app/main/home/map/Map.svelte
@@ -14,39 +14,48 @@
 	let geolocate: GeolocateControl;
 	let scale: ScaleControl;
 
-	let pointsOfInterest: { lngLat: { lng: number; lat: number }; name: string; id: string }[] = [
-		{ lngLat: { lng: -1.617439, lat: 54.978252 }, name: 'Newcastle upon Tyne', id: 'newcastle' },
-		{ lngLat: { lng: -1.617439, lat: 53.800755 }, name: 'Leeds', id: 'leeds' },
-		{ lngLat: { lng: -1.464544, lat: 53.381129 }, name: 'Sheffield', id: 'sheffield' },
-		{ lngLat: { lng: -2.242631, lat: 53.480759 }, name: 'Manchester', id: 'manchester' },
-		{ lngLat: { lng: -2.978481, lat: 53.408371 }, name: 'Liverpool', id: 'liverpool' },
-		{ lngLat: { lng: -1.890401, lat: 52.486243 }, name: 'Birmingham', id: 'birmingham' },
-		{ lngLat: { lng: -1.254341, lat: 51.752022 }, name: 'Oxford', id: 'oxford' },
-		{ lngLat: { lng: -0.127758, lat: 51.507351 }, name: 'London', id: 'london' },
-		{ lngLat: { lng: 1.297355, lat: 52.630885 }, name: 'Norwich', id: 'norwich' },
-		{ lngLat: { lng: -0.139475, lat: 50.82253 }, name: 'Brighton', id: 'brighton' },
-		{ lngLat: { lng: -1.40435, lat: 50.9097 }, name: 'Southampton', id: 'southampton' },
-		{ lngLat: { lng: -2.58791, lat: 51.454513 }, name: 'Bristol', id: 'bristol' },
-		{ lngLat: { lng: -3.188267, lat: 51.481583 }, name: 'Cardiff', id: 'cardiff' },
-		{ lngLat: { lng: -3.939813, lat: 51.621441 }, name: 'Swansea', id: 'swansea' },
-		{ lngLat: { lng: -4.142671, lat: 50.375456 }, name: 'Plymouth', id: 'plymouth' },
-		{ lngLat: { lng: -3.532631, lat: 50.718412 }, name: 'Exeter', id: 'exeter' },
-		{ lngLat: { lng: -2.361324, lat: 51.10914 }, name: 'Glastonbury', id: 'glastonbury' },
-		{ lngLat: { lng: -1.82621, lat: 51.454264 }, name: 'Reading', id: 'reading' },
-		{ lngLat: { lng: -0.97813, lat: 51.454264 }, name: 'Basingstoke', id: 'basingstoke' },
-		{ lngLat: { lng: -0.75557, lat: 51.335345 }, name: 'Guildford', id: 'guildford' },
-		{ lngLat: { lng: -0.376288, lat: 51.41233 }, name: 'Kingston upon Thames', id: 'kingston' },
-		{ lngLat: { lng: 0.278154, lat: 51.44142 }, name: 'Maidstone', id: 'maidstone' },
-		{ lngLat: { lng: 0.52249, lat: 51.270363 }, name: 'Tunbridge Wells', id: 'tunbridge' },
-		{ lngLat: { lng: 1.07561, lat: 51.278707 }, name: 'Canterbury', id: 'canterbury' },
-		{ lngLat: { lng: 1.17432, lat: 51.35996 }, name: 'Margate', id: 'margate' },
-		{ lngLat: { lng: 1.408852, lat: 50.9097 }, name: 'Dover', id: 'dover' },
-		{ lngLat: { lng: 0.73507, lat: 51.656489 }, name: 'Chelmsford', id: 'chelmsford' },
-		{ lngLat: { lng: 0.478913, lat: 51.584151 }, name: 'Brentwood', id: 'brentwood' },
-		{ lngLat: { lng: 0.267459, lat: 51.588638 }, name: 'Romford', id: 'romford' },
-		{ lngLat: { lng: 0.0799, lat: 51.574244 }, name: 'Dagenham', id: 'dagenham' },
-		{ lngLat: { lng: -1.48985, lat: 53.38247}, name: 'Weston Park', id: 'westonparksheffield'}
+
+	let pointsOfInterest: { 
+    lngLat: { lng: number; lat: number }; 
+    name: string; 
+    id: string; 
+    description: string; 
+    features: string[] 
+	}[] = [
+		{ lngLat: { lng: -1.617439, lat: 54.978252 }, name: 'Newcastle upon Tyne', id: 'newcastle', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.617439, lat: 53.800755 }, name: 'Leeds', id: 'leeds', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.464544, lat: 53.381129 }, name: 'Sheffield', id: 'sheffield', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -2.242631, lat: 53.480759 }, name: 'Manchester', id: 'manchester', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -2.978481, lat: 53.408371 }, name: 'Liverpool', id: 'liverpool', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.890401, lat: 52.486243 }, name: 'Birmingham', id: 'birmingham', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.254341, lat: 51.752022 }, name: 'Oxford', id: 'oxford', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -0.127758, lat: 51.507351 }, name: 'London', id: 'london', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 1.297355, lat: 52.630885 }, name: 'Norwich', id: 'norwich', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -0.139475, lat: 50.82253 }, name: 'Brighton', id: 'brighton', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.40435, lat: 50.9097 }, name: 'Southampton', id: 'southampton', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -2.58791, lat: 51.454513 }, name: 'Bristol', id: 'bristol', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -3.188267, lat: 51.481583 }, name: 'Cardiff', id: 'cardiff', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -3.939813, lat: 51.621441 }, name: 'Swansea', id: 'swansea', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -4.142671, lat: 50.375456 }, name: 'Plymouth', id: 'plymouth', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -3.532631, lat: 50.718412 }, name: 'Exeter', id: 'exeter', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -2.361324, lat: 51.10914 }, name: 'Glastonbury', id: 'glastonbury', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.82621, lat: 51.454264 }, name: 'Reading', id: 'reading', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -0.97813, lat: 51.454264 }, name: 'Basingstoke', id: 'basingstoke', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -0.75557, lat: 51.335345 }, name: 'Guildford', id: 'guildford', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -0.376288, lat: 51.41233 }, name: 'Kingston upon Thames', id: 'kingston', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.278154, lat: 51.44142 }, name: 'Maidstone', id: 'maidstone', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.52249, lat: 51.270363 }, name: 'Tunbridge Wells', id: 'tunbridge', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 1.07561, lat: 51.278707 }, name: 'Canterbury', id: 'canterbury', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 1.17432, lat: 51.35996 }, name: 'Margate', id: 'margate', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 1.408852, lat: 50.9097 }, name: 'Dover', id: 'dover', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.73507, lat: 51.656489 }, name: 'Chelmsford', id: 'chelmsford', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.478913, lat: 51.584151 }, name: 'Brentwood', id: 'brentwood', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.267459, lat: 51.588638 }, name: 'Romford', id: 'romford', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: 0.0799, lat: 51.574244 }, name: 'Dagenham', id: 'dagenham', description: 'dummy desc', features: [] },
+		{ lngLat: { lng: -1.48985, lat: 53.38247}, name: 'Weston Park', id: 'westonparksheffield', description: 'Tranquil urban park area offering picnic areas, a bandstand, statues & a duck & fish pond.', features: ["Wheelchair-accessible car park", "Wheelchair-accessible entrance", "Has a tennis court", "Public toilet", "Good for kids", "Dogs allowed"] }
 	];
+
+
 	let idOfSelectedPOI: string | null = null;
 
 	let defaultCoords = { lon: 0, lat: 0 };
@@ -131,10 +140,13 @@
         // Update the state or perform any necessary action
     }
 
-    function navigate() {
-        console.log("Navigate to the POI");
-        // Perform navigation action
-    }
+    // function navigate() {
+    //     console.log("Navigate to the POI");
+    //     // Perform navigation action
+	// 	const { lng, lat } = poi.lngLat;
+    //     const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+    //     window.open(url, '_blank');
+    // }
 </script>
 
 <svelte:window on:resize={() => map?.resize()} />
@@ -173,37 +185,6 @@
 		{@const poi = getPOIById(idOfSelectedPOI)}
 		<PoIcard 
 		closePOICard={closePOICard}
-    	navigate={navigate}
 		{poi} />
-		<!-- <div
-			class="absolute w-56 h-64 top-2 right-2 overflow-y-auto flex"
-			transition:fly={{ duration: 300, x: 200, y: 0, opacity: 0, easing: quintOut }}
-		>
-			<Card.Root class="flex flex-col grow">
-				<Card.Header class="p-3">
-					<Card.Title>{poi.name}</Card.Title>
-				</Card.Header>
-				<Card.Content class="grow p-3 flex flex-col">
-					<p>ID: {poi.id}</p>
-					<p>Longitude: {poi.lngLat.lng}</p>
-					<p>Latitude: {poi.lngLat.lat}</p>
-				</Card.Content>
-				<Card.Footer class="flex justify-between p-3 grow-0 gap-2">
-					<Button
-						size="sm"
-						class="basis-1/2"
-						on:click={() => {
-							console.log('Open up Google Maps and do other stuff...');
-						}}>Navigate</Button
-					>
-					<Button
-						variant="secondary"
-						size="sm"
-						class="basis-1/2"
-						on:click={() => (idOfSelectedPOI = null)}>Close</Button
-					>
-				</Card.Footer>
-			</Card.Root>
-		</div> -->
 	{/if}
 {/if}
