bc56328f0c68ee7cfe87e40c196177c2cb319fcb
Jedrzej Golebiewski <jgolebiewski1@sheffield.ac.uk>
2024-04-30 15:22:27 +0100

    Fix migrations

--- db/migrate/20240430141552_set_default_nickname_description_users.rb
+++
@@ -1,6 +0,0 @@
-class SetDefaultNicknameDescriptionUsers < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :users, :nickname, 'DEFAULT'
-    change_column_default :users, :description, 'DEFAULT'
-  end
-end
